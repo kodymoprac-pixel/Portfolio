c6a5871e4225f0c8d57d32f479436a6cb073cbf0
kodymoprac-pixel <kodymo.prac@gmail.com>
2025-11-08 00:31:13 +0100

    Přepsání vzdáleného repozitáře lokálním obsahem

--- generate_gallery.py
+++ generate_gallery.py
@@ -12,7 +12,6 @@ for folder in os.listdir(images_dir):
     if os.path.isdir(folder_path):
         files = [f for f in os.listdir(folder_path) if f.lower().endswith((".jpg", ".jpeg", ".png", ".gif", ".webp", ".avif"))]
         if files:
-            # relativní cesty, aby fungovalo na GitHub Pages
             gallery[folder] = [f"{images_dir}/{folder}/{f}" for f in files]
 
 # vytvoří gallery_list.js
